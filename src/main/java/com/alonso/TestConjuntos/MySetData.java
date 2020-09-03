004434113e218de0bc553765e9bba0b2f5a252fc
aironman <alonsoir@gmail.com>
2020-09-03 21:36:31 +0200

    added explanation about the use of transient.

--- src/main/java/com/alonso/TestConjuntos/MySetData.java
+++ src/main/java/com/alonso/TestConjuntos/MySetData.java
@@ -9,6 +9,9 @@ import java.util.Set;
 public class MySetData<E> extends AbstractSet<E> implements Set<E>,Cloneable, java.io.Serializable {
 
 	private static final long serialVersionUID = 4546602283055667750L;
+	// en caso de que serialicemos el objeto que contiene esa variable transient, su valor no se serializará.
+	// es decir, en caso de que querramos guardar a disco el contenido de este objeto, cuando querramos recuperarlo,
+	// esos datos no estarán.
 	private transient Map<E,Object> myMap;
 	private Object DUMMYOBJECT = new Object();
 	
